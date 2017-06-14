b73e31702d193bea0f9006d04d39ad0a1902a3b9
Eric Schwartz <eschwartz@Erics-MBP-2.lan>
2017-06-14 13:37:55 -0400

    added class enpoint and model data

---
+++ api/models/Class.js
@@ -0,0 +1,28 @@
+/**
+ * Class.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    class_id: {
+      type: 'integer',
+      primaryKey: true,
+      autoIncrement: true
+    },
+    instructor_id: {
+      type: 'integer'
+    },
+    subject: {
+      type: 'string',
+      maxLength: 30
+    },
+    course: {
+      type: 'integer',
+      maxLength: 4
+    }
+  }
+};
